9a683a86eb5d05a4f0825cce849db36380d0effc
hayukleung <hayukleung@gmail.com>
2016-09-08 17:21:33 +0800

    时钟小部件时针分针角度精确至秒数

--- BeQuiet/src/main/java/com/hayukleung/bequiet/ui/widget/clock/ClockService.java
+++ BeQuiet/src/main/java/com/hayukleung/bequiet/ui/widget/clock/ClockService.java
@@ -4,13 +4,13 @@ import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.IBinder;
 import android.widget.RemoteViews;
-import com.hayukleung.base.ResourceManager;
 import com.hayukleung.bequiet.R;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -23,35 +23,65 @@ import java.util.TimerTask;
  * at 2016-08-30 16:44
  */
 public class ClockService extends Service {
-  private Timer timer;
+
+  private Timer mTimer;
+
+  private Bitmap mHandS;
+  private Bitmap mHandM;
+  private Bitmap mHandH;
 
   @Override public void onCreate() {
     super.onCreate();
-    timer = new Timer();
-    timer.schedule(new MyTimerTask(), 0, 1000);
+
+    mTimer = new Timer();
+    mTimer.schedule(new MyTimerTask(), 0, 1000);
   }
 
   private final class MyTimerTask extends TimerTask {
 
     @Override public void run() {
 
-      Date date = new Date();
-
-      String h = new SimpleDateFormat("hh", Locale.CHINA).format(date);
-      String m = new SimpleDateFormat("mm", Locale.CHINA).format(date);
-      String s = new SimpleDateFormat("ss", Locale.CHINA).format(date);
-
+      // schema 0 使用drawable旋转
+      // Date date = new Date();
+      // String h = new SimpleDateFormat("hh", Locale.CHINA).format(date);
+      // String m = new SimpleDateFormat("mm", Locale.CHINA).format(date);
+      // String s = new SimpleDateFormat("ss", Locale.CHINA).format(date);
       // 获取Widgets管理器
       AppWidgetManager widgetManager = AppWidgetManager.getInstance(getApplicationContext());
       // widgetManager所操作的Widget对应的远程视图即当前Widget的layout文件
       RemoteViews remoteView =
           new RemoteViews(getPackageName(), R.layout.app_widget_clock);
-      // TODO
-      // remoteView.setTextViewText(R.id.time, time);
+      // remoteView.setImageViewResource(R.id.time_s, ResourceManager.getDrawableResId(getApplicationContext().getPackageName(), String.format("time_sec_%s", s)));
+      // remoteView.setImageViewResource(R.id.time_m, ResourceManager.getDrawableResId(getApplicationContext().getPackageName(), String.format("time_min_%s", m)));
+      // remoteView.setImageViewResource(R.id.time_h, ResourceManager.getDrawableResId(getApplicationContext().getPackageName(), String.format("time_hour_%s", h)));
 
-      remoteView.setImageViewResource(R.id.time_h, ResourceManager.getDrawableResId(getApplicationContext().getPackageName(), String.format("time_hour_%s", h)));
-      remoteView.setImageViewResource(R.id.time_m, ResourceManager.getDrawableResId(getApplicationContext().getPackageName(), String.format("time_min_%s", m)));
-      remoteView.setImageViewResource(R.id.time_s, ResourceManager.getDrawableResId(getApplicationContext().getPackageName(), String.format("time_sec_%s", s)));
+      // schema 1 使用bitmap旋转
+      Calendar calendar = Calendar.getInstance();
+      int rawS = calendar.get(Calendar.SECOND);
+      int rawM = calendar.get(Calendar.MINUTE);
+      int rawH = calendar.get(Calendar.HOUR);
+
+      float realS = rawS;
+      float realM = rawM + realS / 60.0f;
+      float realH = rawH + realM / 60.0f;
+
+      float rotateS = 360f / 60f * realS;
+      float rotateM = 360f / 60f * realM;
+      float rotateH = 360f / 12f * realH;
+
+      if (null == mHandS || mHandS.isRecycled()) {
+        mHandS = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.widget_sec_00);
+      }
+      if (null == mHandM || mHandM.isRecycled()) {
+        mHandM = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.widget_min_00);
+      }
+      if (null == mHandH || mHandH.isRecycled()) {
+        mHandH = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.widget_hour_00);
+      }
+
+      remoteView.setImageViewBitmap(R.id.time_s, rotateBitmap(mHandS, rotateS));
+      remoteView.setImageViewBitmap(R.id.time_m, rotateBitmap(mHandM, rotateM));
+      remoteView.setImageViewBitmap(R.id.time_h, rotateBitmap(mHandH, rotateH));
 
       // 当点击Widgets时触发的世界
       // remoteView.setOnClickPendingIntent(viewId, pendingIntent)
@@ -62,12 +92,42 @@ public class ClockService extends Service {
   }
 
   @Override public void onDestroy() {
-    timer.cancel();
-    timer = null;
+    if (null != mHandS && !mHandS.isRecycled()) {
+      mHandS.recycle();
+      mHandS = null;
+    }
+    if (null != mHandM && !mHandM.isRecycled()) {
+      mHandM.recycle();
+      mHandM = null;
+    }
+    if (null != mHandH && !mHandH.isRecycled()) {
+      mHandH.recycle();
+      mHandH = null;
+    }
+    mTimer.cancel();
+    mTimer = null;
     super.onDestroy();
   }
 
   @Override public IBinder onBind(Intent intent) {
     return null;
+  }
+
+  /**
+   * 旋转
+   *
+   * @param source
+   * @param degree
+   * @return
+   */
+  private Bitmap rotateBitmap(Bitmap source, float degree) {
+    if (null == source) {
+      return null;
+    }
+    int size = source.getWidth();
+    Matrix matrix = new Matrix();
+    matrix.reset();
+    matrix.setRotate(degree, size / 2, size / 2);
+    return Bitmap.createBitmap(source, 0, 0, size, size, matrix, true);
   }
 }
